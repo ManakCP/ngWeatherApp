a90141778537027bea054a88ea6fd7350ff26999
manak prajapati <manakchand@outlook.com>
2020-02-02 13:10:51 +0530

    weather service added

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,3 +1,4 @@
 export class Config {
-    service_api = 'api.openweathermap.org/data/2.5/weather?q={city}&APPID=88dc251c19359890486549fc1a7f4bc7';
+    service_key = '88dc251c19359890486549fc1a7f4bc7';
+    service_api = 'api.openweathermap.org/data/2.5/weather?q={city}&APPID=';
 }
